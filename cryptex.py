f7ce814c8b668497e9b5d2fe5394ce38f5cee134
Tobi F <107394980+tf4482@users.noreply.github.com>
2024-02-22 02:27:24 +0100

    Now multiple files can be de- or encrypted via arguments

--- cryptex.py
+++ cryptex.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
 import base64
 import os
+import sys
+
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -17,48 +21,42 @@ def generate_key(password):
     key = base64.urlsafe_b64encode(kdf.derive(password))
     return key
 
-def encrypt(password, file_in, file_out):
-    key = generate_key(password)
+def is_encrypted(file_path):
+    with open(file_path, 'rb') as f:
+        first_line = f.readline()
+        return first_line.startswith(b"gAAAAA")
+
+def encrypt(file_path):
     f = Fernet(key)
-    with open(file_in, 'rb') as f_in:
+    with open(file_path, 'rb') as f_in:
         data = f_in.read()
     encrypted_data = f.encrypt(data)
-    with open(file_out, 'wb') as f_out:
+    with open(file_path, 'wb') as f_out:
         f_out.write(encrypted_data)
 
-def decrypt(password, file_in, file_out):
-    key = generate_key(password)
+def decrypt(file_path):
     f = Fernet(key)
-    with open(file_in, 'rb') as f_in:
+    with open(file_path, 'rb') as f_in:
         data = f_in.read()
     decrypted_data = f.decrypt(data)
-    with open(file_out, 'wb') as f_out:
+    with open(file_path, 'wb') as f_out:
         f_out.write(decrypted_data)
 
-while True:
-    print('''
-    1. Encrypt
-    2. Decrypt
-    3. Exit
-    ''')
-    user_choice = input("Enter your choice: ")
+key = generate_key(input("Enter the Password: "))
 
-    if "3" in user_choice:
-        exit()
-    elif "1" in user_choice:
-        print("Encryption")
-        print("make sure the file is in same directory")
-        originalfilename = input("Enter file name: ")
-        encryptedfilename = input("Enter the name you want to give your encrypted file: ")
-        password = input("Enter the Password: ")
-        encrypt(password, originalfilename, encryptedfilename)
+if sys.argv[1] == "enc" or sys.argv[1] == "dec":
+    i = 2
+else:
+    i = 1
+
+if len(sys.argv) < 2:
+    print("Usage: python script.py <file_path>")
+    sys.exit(1)
+
+for file_path in sys.argv[i:]:
+    if is_encrypted(file_path) and sys.argv[1] != "enc":
+        decrypt(file_path)
+        print("Decryption Done")
+    elif sys.argv[1] != "dec":
+        encrypt(file_path)
         print("Encryption Done")
-    elif "2" in user_choice:
-        print("Decryption")
-        originalfilename = input("Enter file name: ")
-        decryptedfilename = input("Enter the name you want to give your decrypted file: ")
-        password = input("Enter the Password: ")
-        decrypt(password, originalfilename, decryptedfilename)
-    else:
-        print("Please enter a Valid Option")
-
